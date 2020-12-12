365d136d5e137fb850dd9bc87dc9cd542b19ad1c
Dilrukshan Figurado <dilrukshanf@gmail.com>
2020-12-12 11:17:35 +0530

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,11 @@ namespace API
             services.AddDbContext<DataContext>(opt => {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -43,14 +48,10 @@ namespace API
 
             //app.UseHttpsRedirection();
 
+            app.UseCors("CorsPolicy");
             app.UseRouting();
-
             app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
